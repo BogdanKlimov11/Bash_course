bf5a4a425ef21f7b37de716893d9154886f41e47
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-08 13:21:44 +0300

    Create Help.ps1

---
+++ Help.ps1
@@ -0,0 +1,8 @@
+# Получить общую справку по PowerShell
+Get-Help
+
+# Получить справку по командлету Get-Content
+Get-Help Get-Content
+
+# Получить справку по командлету Get-Help
+Get-Help Get-Help
