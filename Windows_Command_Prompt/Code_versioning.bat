a988cfd6f056faf49eed79e3faea135385be1144
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 13:25:10 +0300

    Create Code_versioning.bat

---
+++ Windows_Command_Prompt/Code_versioning.bat
@@ -0,0 +1,11 @@
+:: Клонирование репозитория Git
+git clone https://github.com/user/repo.git
+
+:: Добавление всех изменений в индекс
+git add .
+
+:: Создание коммита с сообщением
+git commit -m "Commit message"
+
+:: Отправка изменений на удаленный репозиторий
+git push origin master
