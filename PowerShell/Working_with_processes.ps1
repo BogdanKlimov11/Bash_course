b2bdb4f7856e22bb11d0b1ca5386bd9d8f9f28af
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 13:07:03 +0300

    Create Working_with_processes.ps1

---
+++ PowerShell/Working_with_processes.ps1
@@ -0,0 +1,11 @@
+# Вывод списка запущенных процессов
+Get-Process
+
+# Завершение процесса по его имени
+Stop-Process -Name "process_name"
+
+# Запуск сервиса или приложения
+Start-Service -Name "service_name"
+
+# Перезагрузка системы
+Restart-Computer
