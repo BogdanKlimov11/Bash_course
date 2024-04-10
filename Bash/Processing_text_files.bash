b23ad61b435de0877b7ddaed7b6db58e1955142b
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 12:57:56 +0300

    Create Processing_text_files.bash

---
+++ Bash/Processing_text_files.bash
@@ -0,0 +1,11 @@
+# Поиск и вывод строк, содержащих определенное слово
+grep "search_term" my_file.txt
+
+# Замена всех вхождений одного слова на другое
+sed -i 's/old_word/new_word/g' my_file.txt
+
+# Извлечение первых 10 строк из файла
+head -n 10 my_file.txt
+
+# Объединение содержимого двух файлов в один
+cat file1.txt file2.txt > merged_file.txt
