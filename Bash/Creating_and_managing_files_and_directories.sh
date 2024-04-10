5e21649c3a8cf971e57fd8d83cd0ba851f37e52b
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 12:56:42 +0300

    Create Creating_and_managing_files_and_directories.sh

---
+++ Bash/Creating_and_managing_files_and_directories.sh
@@ -0,0 +1,17 @@
+# Создание папки
+mkdir my_folder
+
+# Создание файла
+touch my_file.txt
+
+# Копирование файла
+cp my_file.txt new_file.txt
+
+# Перемещение файла
+mv my_file.txt /path/to/new_location/
+
+# Удаление файла
+rm my_file.txt
+
+# Удаление папки (со всем ее содержимым)
+rm -r my_folder
