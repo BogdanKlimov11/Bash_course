372a74b8e70eddd521d540c261767c0e4ff1f083
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 12:59:29 +0300

    Create Network_Operations.bash

---
+++ Bash/Network_Operations.bash
@@ -0,0 +1,8 @@
+# Проверка доступности хоста по его IP-адресу
+ping 192.168.1.1
+
+# Проверка маршрутизации к хосту
+traceroute example.com
+
+# Копирование файла на удаленный сервер по SSH
+scp file.txt user@example.com:/path/to/destination/
