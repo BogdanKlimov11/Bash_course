dd58dd21a4c21069247842824c8143a0bfa5b9cc
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 13:08:03 +0300

    Create Automation_of_tasks.ps1

---
+++ PowerShell/Automation_of_tasks.ps1
@@ -0,0 +1,6 @@
+# Создание скрипта и его выполнение
+echo "Write-Host 'Hello, world!'" > script.ps1
+.\script.ps1
+
+# Запуск скрипта по расписанию
+# В PowerShell нет аналога cron, но можно использовать планировщик задач Windows.
