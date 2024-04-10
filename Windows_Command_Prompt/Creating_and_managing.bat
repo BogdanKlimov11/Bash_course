86b31d79b44c698fa79fd3e5bf1f88cc74f20dbb
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 13:16:23 +0300

    Create Creating_and_managing.bat

---
+++ Windows_Command_Prompt/Creating_and_managing.bat
@@ -0,0 +1,17 @@
+:: Создание папки
+mkdir my_folder
+
+:: Создание файла
+echo. > my_file.txt
+
+:: Копирование файла
+copy my_file.txt new_file.txt
+
+:: Перемещение файла
+move my_file.txt C:\path\to\new_location\
+
+:: Удаление файла
+del my_file.txt
+
+:: Удаление папки (со всем ее содержимым)
+rmdir /s /q my_folder
