6826cddaf3e89555d1b063f5584c05708af0cb35
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-04-10 13:03:13 +0300

    Delete Help.ps1

--- Help.ps1
+++
@@ -1,8 +0,0 @@
-# Получить общую справку по PowerShell
-Get-Help
-
-# Получить справку по командлету Get-Content
-Get-Help Get-Content
-
-# Получить справку по командлету Get-Help
-Get-Help Get-Help
